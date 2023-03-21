a3a9c59acc69f9d0eff53bc3212d2d748ef27974
Trung Dao Quoc <trung.daoquoc@gameloft.com>
2023-03-21 18:46:25 +0700

    update login and login , sign up

--- app/src/main/java/com/ET/wetalk/MainActivity.java
+++ app/src/main/java/com/ET/wetalk/MainActivity.java
@@ -1,14 +1,72 @@
 package com.ET.wetalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.ET.wetalk.activities.SignInActivity;
+import com.ET.wetalk.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        init();
+        setListeners();
+        setup();
+    }
+
+    private void init()
+    {
+        auth= FirebaseAuth.getInstance();
+    }
+
+    private void setListeners()
+    {
+
+    }
+
+    private void setup()
+    {
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.settings:
+                Toast.makeText(this,"Settings Clicked",Toast.LENGTH_SHORT).show();
+            case R.id.logout:
+                auth.signOut();
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/ET/wetalk/adapters/FragmentsAdapter.java
@@ -0,0 +1,23 @@
+package com.ET.wetalk.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class FragmentsAdapter extends FragmentPagerAdapter {
+    public FragmentsAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
